8e0588f03251b5f1552c6897ce50c44f3586e2c1
Dailyscat <pay_nack@naver.com>
2019-04-27 20:52:00 +0900

    vanillaSpread Init

---
+++ config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  url: 'https://vanillaroad.dev',
+  title: 'Vanilla Road',
+  subtitle: 'Vanilla Webtech is Future.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About',
+      path: '/pages/about'
+    },
+    {
+      label: 'Archive',
+      path: '/pages/archive'
+    },
+    {
+      label: 'Contact',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Vanilla Road',
+    photo: '/photo.png',
+    bio: 'Knowing Vanilla Webtech will make you a key engineer who can solve complex problems',
+    contacts: {
+      email: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '#',
+      vkontakte: '#'
+    }
+  }
+};
---
+++ content/pages/about.md
@@ -0,0 +1,17 @@
+---
+title: "About"
+template: "page"
+---
+
+##바로 걷는 개발자
+<br/>
+<br/>
+바닐라로는 Vanilla Webtechnology, 기본에 중점을 두고 함께 성장해가는 개발자 커뮤니티입니다.
+
+프로젝트에서의 트러블 슈팅 경험, 웹 표준 기술에 대한 지식 혹은 혼자 알기 아까운 꿀팁까지 가볍게 공유할 수 있는 모임이 되고자 합니다. 웹 개발의 길을 모두와 함께, 더 멀리 걸어가고 싶습니다.
+
+![Knowing Vanilla Webtech will make you a key engineer who can solve complex problems](/media/image-2.jpg)
+
+***
+
+**문의: dailyscat@gmail.com**
